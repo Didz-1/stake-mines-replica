d6f08be7a29b80bafb74e21ea58cab1e49436870
Adil Wadud <adilwadud10@gmail.com>
2023-11-27 00:00:17 +0000

    multiplier working

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const rows = 5;
 const columns = 5;
-//let board = [];
 let board = {};
+let currentBalance = 0;
+let mines = 1;
+let diamonds = 0;
 
 
 
@@ -9,18 +11,27 @@ let board = {};
 function startGame() {
     //create the board
     for (let r=0; r<rows; r++) {
-        //let row = [];
         for (let c=0; c<columns; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList = "unchecked tile"
             document.querySelector(".grid").append(tile);
-            //row.push(`${r.toString()}-${c.toString()}`);
             board[`${r.toString()}-${c.toString()}`] = false;
         }
-        //board.push(row);
     }
     console.log(board);
+
+
+
+    //set balance
+    let wallet = document.querySelector(".balance");
+    let walletBalance = document.createElement("h3");
+    walletBalance.textContent = `Balance: ${currentBalance}`;
+    wallet.appendChild(walletBalance);
+
+    setMultiplier(1);
+
+
 }
 startGame();
 
@@ -68,6 +79,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
                     e.classList.remove("unchecked");
                     e.classList.add("checked");
+
+                    //update multiplier
+                    diamonds++;
+                    const multiplier = (calculateMultiplier(mines, diamonds)).toFixed(2);
+                    updateMultiplier(multiplier);
                 }
             }
             
@@ -80,3 +96,37 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+//multiplier calc
+function nCr(n, r) {
+    const factorial = (num) => (num <= 1 ? 1 : num * factorial(num - 1));
+    return factorial(n) / (factorial(r) * factorial(n - r));
+}
+  
+function calculateMultiplier(mines, diamonds) {
+    const houseEdge = 0.01;
+    return (1 - houseEdge) * nCr(25, diamonds) / nCr(25 - mines, diamonds);
+}
+  
+
+
+
+//set multiplier
+function setMultiplier(currentMultiplier) {
+    let multiplier = document.querySelector(".multiplier");
+    let multiplierNum = document.createElement("h3");
+    multiplierNum.classList.add("multiplier-num");
+    multiplierNum.textContent = `Multiplier: x${currentMultiplier}`;
+    multiplier.appendChild(multiplierNum);
+}
+
+//update multiplier
+function updateMultiplier(currentMultiplier) {
+    let multiplier = document.querySelector(".multiplier-num");
+    multiplier.textContent = `Multiplier: x${currentMultiplier}`;
+    
+}
+
+
